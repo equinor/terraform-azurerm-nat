c5330ca8c136860cf11df66f9d0b81acdbe8740a
Henrik Knutsen <46495473+hknutsen@users.noreply.github.com>
2023-12-12 09:54:29 +0000

    feat!: dont create subnet association

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -12,28 +12,12 @@ resource "random_id" "example" {
   byte_length = 8
 }
 
-module "network" {
-  source = "github.com/equinor/terraform-azurerm-network?ref=v2.0.0"
+module "public_ip" {
+  source = "github.com/equinor/terraform-azurerm-public-ip?ref=v0.1.0"
 
-  vnet_name           = "vnet-${random_id.example.hex}"
+  address_name        = "pip-${random_id.example.hex}"
   resource_group_name = var.resource_group_name
   location            = var.location
-  address_spaces      = ["10.0.0.0/16"]
-
-  subnets = {
-    "example" = {
-      name             = "snet-${random_id.example.hex}"
-      address_prefixes = ["10.0.1.0/24"]
-    }
-  }
-}
-
-resource "azurerm_public_ip" "example" {
-  name                = "pip-${random_id.example.hex}"
-  resource_group_name = var.resource_group_name
-  location            = var.location
-  sku                 = "Standard"
-  allocation_method   = "Static"
 
   tags = local.tags
 }
@@ -42,12 +26,10 @@ module "nat" {
   # source = "github.com/equinor/terraform-azurerm-nat?ref=v0.0.0"
   source = "../.."
 
-  gateway_name        = "ng-${random_id.example.hex}"
-  resource_group_name = var.resource_group_name
-  location            = var.location
-
-  subnet_ids            = [module.network.subnet_ids["example"]]
-  public_ip_address_ids = [azurerm_public_ip.example.id]
+  gateway_name          = "ng-${random_id.example.hex}"
+  resource_group_name   = var.resource_group_name
+  location              = var.location
+  public_ip_address_ids = [module.public_ip.address_id]
 
   tags = local.tags
 }
