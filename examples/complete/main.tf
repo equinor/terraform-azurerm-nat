190cbed647cac027607189771a56a1ac824cd917
Henrik Knutsen <46495473+hknutsen@users.noreply.github.com>
2023-12-15 13:46:29 +0000

    chore: update complete example

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -12,12 +12,21 @@ resource "random_id" "example" {
   byte_length = 8
 }
 
+module "log_analytics" {
+  source = "github.com/equinor/terraform-azurerm-log-analytics?ref=v2.1.1"
+
+  workspace_name      = "log-${random_id.example.hex}"
+  resource_group_name = var.resource_group_name
+  location            = var.location
+}
+
 module "public_ip" {
   source = "github.com/equinor/terraform-azurerm-public-ip?ref=v0.1.0"
 
-  address_name        = "pip-${random_id.example.hex}"
-  resource_group_name = var.resource_group_name
-  location            = var.location
+  address_name               = "pip-${random_id.example.hex}"
+  resource_group_name        = var.resource_group_name
+  location                   = var.location
+  log_analytics_workspace_id = module.log_analytics.workspace_id
 
   tags = local.tags
 }
