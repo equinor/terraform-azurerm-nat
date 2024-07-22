2854c2bf5befd5547d85631af0a50b18d7fbe7d9
Henrik Knutsen <46495473+hknutsen@users.noreply.github.com>
2024-07-22 20:17:07 +0200

    chore: clarify why we don't create subnet association resources

--- main.tf
+++ main.tf
@@ -13,3 +13,10 @@ resource "azurerm_nat_gateway_public_ip_association" "this" {
   nat_gateway_id       = azurerm_nat_gateway.this.id
   public_ip_address_id = var.public_ip_address_ids[count.index]
 }
+
+# Don't create "azurerm_subnet_nat_gateway_association" resources in this module.
+# They should be created in the Azure Network module instead:
+# https://registry.terraform.io/modules/equinor/network/azurerm/latest
+#
+# This ensures a standard approach to associating a subnet with a NAT gateway:
+# a subnet is associated with a NAT gateway during subnet creation.
