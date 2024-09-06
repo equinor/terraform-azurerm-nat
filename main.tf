ad990fec0f8d97e71fc607f1e6ec66f6341d6f00
Henrik Simonsen Knutsen <46495473+hknutsen@users.noreply.github.com>
2024-09-06 09:30:13 +0200

    feat: create Public IP prefix associations (#20)

--- main.tf
+++ main.tf
@@ -14,6 +14,13 @@ resource "azurerm_nat_gateway_public_ip_association" "this" {
   public_ip_address_id = var.public_ip_address_ids[count.index]
 }
 
+resource "azurerm_nat_gateway_public_ip_prefix_association" "this" {
+  count = length(var.public_ip_prefix_ids)
+
+  nat_gateway_id      = azurerm_nat_gateway.this.id
+  public_ip_prefix_id = var.public_ip_prefix_ids[count.index]
+}
+
 # Don't create "azurerm_subnet_nat_gateway_association" resources in this module.
 # They should be created in the Azure Network module instead:
 # https://registry.terraform.io/modules/equinor/network/azurerm/latest
